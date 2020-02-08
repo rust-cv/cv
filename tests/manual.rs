8da8f22de792ab4d5a99cc438fc32254550ca70f
Geordon Worley <vadixidav@gmail.com>
2020-02-07 21:37:39 -0500

    now that no code comes from TheiaSfM anymore, convert license to MIT

--- tests/manual.rs
+++ tests/manual.rs
@@ -14,8 +14,8 @@ const NEAR: f32 = 0.1;
 const EPS_ROTATION: f64 = 1e-13;
 const ITER_ROTATION: usize = 50000;
 
-const EIGHT_POINT_EIGEN_CONVERGENCE: f64 = 1e-12;
-const EIGHT_POINT_EIGEN_ITERATIONS: usize = 500000;
+const EIGHT_POINT_EIGEN_CONVERGENCE: f64 = 1e-4;
+const EIGHT_POINT_EIGEN_ITERATIONS: usize = 50;
 
 fn to_five(a: [NormalizedKeyPoint; SAMPLE_POINTS]) -> [NormalizedKeyPoint; 5] {
     [a[0], a[1], a[2], a[3], a[4]]
