31669a88c9e6253641df9e60da460a8b1c0babaa
Geordon Worley <vadixidav@gmail.com>
2020-02-19 02:14:15 -0500

    extract features in a separate thread with akaze

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use eight_point::EightPoint;
 use rand::SeedableRng;
 use rand_pcg::Pcg64;
 use std::path::PathBuf;
+use std::sync::mpsc::{sync_channel, Receiver};
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
@@ -25,90 +26,99 @@ struct Opt {
     /// The threshold for ARRSAC.
     #[structopt(short, long, default_value = "0.001")]
     arrsac_threshold: f32,
-    /// Input folder with image files
+    /// List of image files
     ///
-    /// Kitti 2011_09_26
+    /// Must be Kitti 2011_09_26 camera 0
     #[structopt(parse(from_os_str))]
-    input: PathBuf,
+    images: Vec<PathBuf>,
 }
 
 fn main() {
     let opt = Opt::from_args();
-    // AKAZE extraction configuration
-    let akaze_config = AkazeConfig::default();
-    let images = opt
-        .input
-        .join("2011_09_26_drive_0035_extract")
-        .join("image_00")
-        .join("data");
-    let paths = vec![images.join("0000000000.png"), images.join("0000000001.png")];
-    let mut image_points = paths
-        .into_iter()
-        .map(|path| akaze::extract_features(path, akaze_config))
-        .map(|(_, kps, descriptors)| {
-            (
-                kps.into_iter()
-                    .map(|kp| ImageKeyPoint(Point2::new(kp.point.0 as f64, kp.point.1 as f64)))
-                    .collect::<Vec<_>>(),
-                descriptors
-                    .into_iter()
-                    .map(|AkazeDescriptor { vector }| vector)
-                    .collect::<Vec<_>>(),
-            )
-        });
     // Intrinsics retrieved from calib_cam_to_cam.txt K_00.
     let intrinsics = CameraIntrinsics {
         focals: Vector2::new(9.842_439e2, 9.808_141e2),
         principal_point: Point2::new(6.9e2, 2.331_966e2),
         skew: 0.0,
     };
-    let (a_kps, a_descriptors) = image_points.next().unwrap();
-    eprintln!("a_kps: {}", a_kps.len());
-    let (b_kps, b_descriptors) = image_points.next().unwrap();
-    eprintln!("b_kps: {}", b_kps.len());
-    let forward_matches = matching(&a_descriptors, &b_descriptors);
-    let reverse_matches = matching(&b_descriptors, &a_descriptors);
-    let matches = forward_matches
-        .iter()
-        .enumerate()
-        .filter_map(|(aix, &(bix, distance))| {
-            let is_symmetric = reverse_matches[bix].0 == aix;
-            let in_threshold = distance < opt.match_threshold;
-            if is_symmetric && in_threshold {
-                let a = intrinsics.normalize(a_kps[aix]);
-                let b = intrinsics.normalize(b_kps[bix]);
-                Some(KeyPointsMatch(a, b))
-            } else {
-                None
-            }
-        })
-        .collect::<Vec<_>>();
-    eprintln!("matches: {}", matches.len());
-    let eight_point = EightPoint::new();
-    let mut arrsac = Arrsac::new(
-        ArrsacConfig::new(opt.arrsac_threshold),
-        Pcg64::from_seed([1; 32]),
-    );
-    let (essential, inliers) = arrsac
-        .model_inliers(&eight_point, matches.iter().copied())
-        .unwrap();
-    eprintln!("inliers: {}", inliers.len());
-    let residual_average = inliers
-        .iter()
-        .map(|&ix| essential.residual(&matches[ix]).abs())
-        .sum::<f32>()
-        / inliers.len() as f32;
-    eprintln!("inlier residual average: {}", residual_average);
-    let pose = essential
-        .solve_unscaled_pose(
-            1e-6,
-            100,
-            0.5,
-            cv_core::geom::make_one_pose_dlt_triangulator(1e-6, 100),
-            matches.iter().copied(),
-        )
-        .unwrap();
-    eprintln!("rotation: {:?}", pose.rotation.angle());
+
+    // Create a channel that will produce features in another parallel thread.
+    let features = features_stream(opt.images.clone());
+    let mut prev = features.recv().unwrap();
+    for next in features {
+        eprintln!("prev kps: {}", prev.0.len());
+        eprintln!("next kps: {}", next.0.len());
+        let forward_matches = matching(&prev.1, &next.1);
+        let reverse_matches = matching(&next.1, &prev.1);
+        let matches = forward_matches
+            .iter()
+            .enumerate()
+            .filter_map(|(aix, &(bix, distance))| {
+                let is_symmetric = reverse_matches[bix].0 == aix;
+                let in_threshold = distance < opt.match_threshold;
+                if is_symmetric && in_threshold {
+                    let a = intrinsics.normalize(prev.0[aix]);
+                    let b = intrinsics.normalize(next.0[bix]);
+                    Some(KeyPointsMatch(a, b))
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>();
+        eprintln!("matches: {}", matches.len());
+        let eight_point = EightPoint::new();
+        let mut arrsac = Arrsac::new(
+            ArrsacConfig::new(opt.arrsac_threshold),
+            Pcg64::from_seed([1; 32]),
+        );
+        let (essential, inliers) = arrsac
+            .model_inliers(&eight_point, matches.iter().copied())
+            .unwrap();
+        eprintln!("inliers: {}", inliers.len());
+        let residual_average = inliers
+            .iter()
+            .map(|&ix| essential.residual(&matches[ix]).abs())
+            .sum::<f32>()
+            / inliers.len() as f32;
+        eprintln!("inlier residual average: {}", residual_average);
+        let pose = essential
+            .solve_unscaled_pose(
+                1e-6,
+                100,
+                0.5,
+                cv_core::geom::make_one_pose_dlt_triangulator(1e-6, 100),
+                matches.iter().copied(),
+            )
+            .unwrap();
+        eprintln!("rotation: {:?}", pose.rotation.angle());
+        prev = next;
+    }
+}
+
+fn features_stream(paths: Vec<PathBuf>) -> Receiver<(Vec<ImageKeyPoint>, Vec<Vec<u8>>)> {
+    let (tx, rx) = sync_channel(5);
+
+    std::thread::spawn(move || {
+        for path in paths {
+            tx.send(kps_descriptors(path)).unwrap();
+        }
+    });
+
+    rx
+}
+
+fn kps_descriptors(path: PathBuf) -> (Vec<ImageKeyPoint>, Vec<Vec<u8>>) {
+    let akaze_config = AkazeConfig::default();
+    let (_, kps, descriptors) = akaze::extract_features(path, akaze_config);
+    (
+        kps.into_iter()
+            .map(|kp| ImageKeyPoint(Point2::new(kp.point.0 as f64, kp.point.1 as f64)))
+            .collect::<Vec<_>>(),
+        descriptors
+            .into_iter()
+            .map(|AkazeDescriptor { vector }| vector)
+            .collect::<Vec<_>>(),
+    )
 }
 
 fn distance(a: &[u8], b: &[u8]) -> u32 {
