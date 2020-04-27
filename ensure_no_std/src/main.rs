28e266f2bb061dee9bca5ae4f0a4778bde5ef35d
Geordon Worley <vadixidav@gmail.com>
2020-04-27 18:56:22 -0400

    add integration tests

---
+++ ensure_no_std/src/main.rs
@@ -0,0 +1,16 @@
+// ensure_no_std/src/main.rs
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
