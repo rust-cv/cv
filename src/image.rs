a25ff0448d95600f10c69acb7e4f7d95045c1293
Geordon Worley <vadixidav@gmail.com>
2020-05-08 18:32:18 -0400

    add info log to tell user whether loaded image is 8-bit or 16-bit

--- src/image.rs
+++ src/image.rs
@@ -1,5 +1,6 @@
 use derive_more::{Deref, DerefMut};
 use image::{imageops, DynamicImage, ImageBuffer, Luma};
+use log::*;
 use ndarray::{Array2, ArrayView2, ArrayViewMut2};
 use nshare::{MutNdarray2, RefNdarray2};
 use std::f32;
@@ -41,11 +42,13 @@ impl GrayFloatImage {
     pub fn from_dynamic(input_image: &DynamicImage) -> Self {
         Self(match input_image.grayscale() {
             DynamicImage::ImageLuma8(gray_image) => {
+                info!("Loaded an 8-bit image");
                 ImageBuffer::from_fn(gray_image.width(), gray_image.height(), |x, y| {
                     Luma([f32::from(gray_image[(x, y)][0]) / 255f32])
                 })
             }
             DynamicImage::ImageLuma16(gray_image) => {
+                info!("Loaded a 16-bit image");
                 ImageBuffer::from_fn(gray_image.width(), gray_image.height(), |x, y| {
                     Luma([f32::from(gray_image[(x, y)][0]) / 65535f32])
                 })
