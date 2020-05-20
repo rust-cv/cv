609ce46f11a3e7c6693b9ca055a39803f3129039
Geordon Worley <vadixidav@gmail.com>
2020-05-20 12:15:04 -0400

    make it work, including with optional output argument

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use akaze::{Akaze, KeyPoint};
-use image::{DynamicImage, ImageOutputFormat, Rgb};
+use image::{DynamicImage, ImageOutputFormat, Rgba};
 use imageproc::drawing;
 use std::path::PathBuf;
 use structopt::StructOpt;
@@ -13,6 +13,11 @@ struct Opt {
     /// The image file to show keypoints on.
     #[structopt(name = "FILE", parse(from_os_str))]
     file: PathBuf,
+    /// The output path to write to (autodetects image type from extension).
+    ///
+    /// If this is not provided, then the output goes to stdout as a PNG.
+    #[structopt(short, long, parse(from_os_str))]
+    output: Option<PathBuf>,
 }
 
 fn main() {
@@ -21,24 +26,21 @@ fn main() {
     let (kps, _) = akaze
         .extract_path(&opt.file)
         .expect("failed to extract features");
-    let mut image = image::open(&opt.file)
-        .expect("failed to open image for drawing")
-        .into_rgb();
-    for KeyPoint {
-        point: (x, y),
-        size,
-        ..
-    } in kps
-    {
-        drawing::draw_filled_circle(
-            &mut image,
-            (x as i32, y as i32),
-            size as i32,
-            Rgb([0, 255, 255]),
-        );
+    let mut image = drawing::Blend(
+        image::open(&opt.file)
+            .expect("failed to open image for drawing")
+            .into_rgba(),
+    );
+    for KeyPoint { point: (x, y), .. } in kps {
+        drawing::draw_cross_mut(&mut image, Rgba([0, 255, 255, 128]), x as i32, y as i32);
     }
     let stdout = std::io::stdout();
-    DynamicImage::ImageRgb8(image)
-        .write_to(&mut stdout.lock(), ImageOutputFormat::Png)
-        .expect("failed to write image to stdout");
+    let image = DynamicImage::ImageRgba8(image.0);
+    if let Some(path) = opt.output {
+        image.save(path).expect("failed to write image to stdout");
+    } else {
+        image
+            .write_to(&mut stdout.lock(), ImageOutputFormat::Png)
+            .expect("failed to write image to stdout");
+    }
 }
