23985a4cbad3da4d08ee8bb80f7b998365598dca
Geordon Worley <vadixidav@gmail.com>
2020-06-06 16:53:08 -0400

    refactor all tracking logic into VSlam

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,20 @@
 mod export;
-mod slam;
+mod vslam;
 
 use cv::{
-    camera::pinhole::{self, CameraIntrinsics, CameraIntrinsicsK1Distortion, NormalizedKeyPoint},
+    camera::pinhole::{CameraIntrinsics, CameraIntrinsicsK1Distortion},
     consensus::Arrsac,
     estimate::EightPoint,
-    feature::akaze,
-    geom::{MinimalSquareReprojectionErrorTriangulator, RelativeDltTriangulator},
-    BitArray, CameraModel, Consensus, FeatureMatch, Model, TriangulatorRelative,
+    geom::MinimalSquareReprojectionErrorTriangulator,
 };
 
-use cv_optimize::TwoViewOptimizer;
-use levenberg_marquardt::LevenbergMarquardt;
-use rand::seq::SliceRandom;
+use cv::nalgebra::{Point2, Vector2};
 
-use cv::nalgebra::{Point2, Point3, Vector2};
-
-use log::*;
 use rand::SeedableRng;
 use rand_pcg::Pcg64;
-use std::path::{Path, PathBuf};
-use std::sync::mpsc::{sync_channel, Receiver};
+use std::path::PathBuf;
 use structopt::StructOpt;
-
-type Descriptor = BitArray<64>;
+use vslam::*;
 
 #[derive(StructOpt, Clone)]
 #[structopt(name = "vslam-sandbox", about = "A tool for testing vslam algorithms.")]
@@ -43,8 +34,8 @@ struct Opt {
     #[structopt(short = "z", long, default_value = "0.001")]
     akaze_threshold: f64,
     /// The threshold for reprojection error in pixels.
-    #[structopt(long, default_value = "2.5")]
-    reprojection_threshold: f64,
+    #[structopt(long, default_value = "0.001")]
+    cosine_distance_threshold: f64,
     /// The x focal length
     #[structopt(long, default_value = "984.2439")]
     x_focal: f64,
@@ -86,190 +77,29 @@ fn main() {
         opt.radial_distortion,
     );
 
-    let net_focal = (opt.x_focal.powi(2) + opt.y_focal.powi(2)).sqrt();
-
     // Create a channel that will produce features in another parallel thread.
-    let features = features_stream(&opt);
-    let mut prev = features.recv().unwrap();
-    for next in features {
-        info!("start frame");
-        info!("prev kps: {}", prev.0.len());
-        info!("next kps: {}", next.0.len());
+    let mut vslam = VSlam::new(
+        Arrsac::new(opt.arrsac_threshold, Pcg64::from_seed([5; 32])),
+        EightPoint::new(),
+        MinimalSquareReprojectionErrorTriangulator::new(),
+        Pcg64::from_seed([5; 32]),
+    )
+    .akaze_threshold(opt.akaze_threshold)
+    .match_threshold(opt.match_threshold)
+    .optimization_points(opt.optimization_points)
+    .cosine_distance_threshold(opt.cosine_distance_threshold);
 
-        // Compute best matches from previous frame to next frame and vice versa.
-        let forward_matches = matching(&prev.1, &next.1);
-        let reverse_matches = matching(&next.1, &prev.1);
+    // Add the feed.
+    let feed = vslam.insert_feed(intrinsics);
 
-        // Compute the symmetric matches (matches that were the same going forwards and backwards).
-        let matches: Vec<(FeatureMatch<usize>, FeatureMatch<NormalizedKeyPoint>)> = forward_matches
-            .iter()
-            .enumerate()
-            .filter_map(|(aix, &(bix, distance))| {
-                let is_symmetric = reverse_matches[bix].0 == aix;
-                let in_threshold = distance < opt.match_threshold;
-                if is_symmetric && in_threshold {
-                    Some((
-                        FeatureMatch(aix, bix),
-                        FeatureMatch(
-                            intrinsics.calibrate(prev.0[aix]),
-                            intrinsics.calibrate(prev.0[bix]),
-                        ),
-                    ))
-                } else {
-                    None
-                }
-            })
-            .collect();
-        info!("matches: {}", matches.len());
-
-        // Perform sample consensus.
-        let eight_point = EightPoint::new();
-        let mut arrsac = Arrsac::new(opt.arrsac_threshold, Pcg64::from_seed([1; 32]));
-        let (essential, geometric_verification_inlier_indices) = arrsac
-            .model_inliers(&eight_point, matches.iter().map(|&(_, m)| m))
-            .unwrap();
-        let matches = geometric_verification_inlier_indices
-            .iter()
-            .map(|&ix| matches[ix])
-            .collect::<Vec<_>>();
-        info!("inliers after sample consensus: {}", matches.len());
-        let residual_average = matches
-            .iter()
-            .map(|(_, m)| essential.residual(m).abs())
-            .sum::<f32>()
-            / matches.len() as f32;
-        info!(
-            "inlier residual average after sample consensus: {}",
-            residual_average
-        );
-
-        // Perform chirality test to determine which essential matrix is correct.
-        let (pose, chirality_inlier_indices) = essential
-            .pose_solver()
-            .solve_unscaled_inliers(
-                MinimalSquareReprojectionErrorTriangulator::new(),
-                matches.iter().map(|&(_, m)| m),
-            )
-            .unwrap();
-        let matches = chirality_inlier_indices
-            .iter()
-            .map(|&inlier| matches[inlier])
-            .collect::<Vec<_>>();
-        info!(
-            "inliers after chirality test: {}",
-            chirality_inlier_indices.len()
-        );
-
-        // Select a random sample of 32 points to use for optimization.
-        let opti_matches = matches
-            .choose_multiple(&mut rand::thread_rng(), opt.optimization_points)
-            .map(|&(_, m)| m)
-            .collect::<Vec<_>>();
-
-        let lm = LevenbergMarquardt::new();
-        let pose = lm
-            .minimize(TwoViewOptimizer::new(
-                opti_matches.iter().copied(),
-                pose.0,
-                RelativeDltTriangulator::new(),
-            ))
-            .0
-            .pose;
-
-        // Filter outlier matches based on reprojection error.
-        let matches: Vec<_> = matches
-            .iter()
-            .copied()
-            .filter(|&(_, m)| {
-                // Get the reprojection error in focal length.
-                let error_in_focal_length = pinhole::average_pose_reprojection_error(
-                    pose,
-                    m,
-                    MinimalSquareReprojectionErrorTriangulator::new(),
-                )
-                .unwrap();
-                // Then convert it to pixels.
-                let error_in_pixels = error_in_focal_length * net_focal;
-
-                debug!("error in pixels: {}", error_in_pixels);
-
-                error_in_pixels < opt.reprojection_threshold
-            })
-            .collect();
-        info!(
-            "inliers after reprojection error filtering: {}",
-            matches.len()
-        );
-        let mean_reprojection_error = matches
-            .iter()
-            .map(|&(_, m)| {
-                // Get the reprojection error in focal length.
-                let error_in_focal_length = pinhole::average_pose_reprojection_error(
-                    pose,
-                    m,
-                    MinimalSquareReprojectionErrorTriangulator::new(),
-                )
-                .unwrap();
-                // Then convert it to pixels.
-                error_in_focal_length * net_focal
-            })
-            .sum::<f64>()
-            / matches.len() as f64;
-        info!(
-            "mean reprojection error after reprojection error filtering: {}",
-            mean_reprojection_error
-        );
-
-        // Output point cloud.
-        if let Some(outpath) = opt.output.as_ref() {
-            let points: Vec<Point3<f64>> = matches
-                .iter()
-                .copied()
-                .map(|(_, FeatureMatch(a, b))| {
-                    MinimalSquareReprojectionErrorTriangulator::new()
-                        .triangulate_relative(pose, a, b)
-                        .unwrap()
-                        .0
-                })
-                .collect();
-            export::export(std::fs::File::create(outpath).unwrap(), points);
-        }
-        info!("rotation: {:?}", pose.rotation.angle());
-        prev = next;
-        info!("end frame");
+    // Add the frames.
+    for path in opt.images {
+        let image = image::open(path).expect("failed to load image");
+        vslam.insert_frame(feed, &image);
     }
-}
 
-fn features_stream(opt: &Opt) -> Receiver<(Vec<akaze::KeyPoint>, Vec<Descriptor>)> {
-    let (tx, rx) = sync_channel(5);
-
-    let opt = opt.clone();
-
-    std::thread::spawn(move || {
-        for path in &opt.images {
-            tx.send(kps_descriptors(path, &opt)).unwrap();
-        }
-    });
-
-    rx
-}
-
-fn kps_descriptors(path: impl AsRef<Path>, opt: &Opt) -> (Vec<akaze::KeyPoint>, Vec<Descriptor>) {
-    akaze::Akaze::new(opt.akaze_threshold)
-        .extract_path(path)
-        .unwrap()
-}
-
-fn matching(a_descriptors: &[Descriptor], b_descriptors: &[Descriptor]) -> Vec<(usize, usize)> {
-    a_descriptors
-        .iter()
-        .map(|a| {
-            b_descriptors
-                .iter()
-                .map(|b| a.distance(&b))
-                .enumerate()
-                .min_by_key(|&(_, d)| d)
-                .unwrap()
-        })
-        .collect::<Vec<_>>()
+    // Export the first match
+    if let Some(path) = opt.output {
+        vslam.export_covisibility(Pair::new(0, 1), path);
+    }
 }
---
+++ src/vslam.rs
@@ -0,0 +1,411 @@
+use cv::nalgebra::Point3;
+use cv::{
+    camera::pinhole::{CameraIntrinsicsK1Distortion, NormalizedKeyPoint},
+    feature::akaze,
+    Bearing, BitArray, CameraModel, Consensus, EssentialMatrix, Estimator, FeatureMatch, Pose,
+    RelativeCameraPose, TriangulatorObservances, TriangulatorRelative,
+};
+use cv_optimize::TwoViewOptimizer;
+use image::DynamicImage;
+use levenberg_marquardt::LevenbergMarquardt;
+use rand::{seq::SliceRandom, Rng};
+use slab::Slab;
+use std::cell::RefCell;
+use std::collections::{HashMap, HashSet};
+use std::path::Path;
+
+type Features = Vec<(NormalizedKeyPoint, BitArray<64>)>;
+
+struct Frame {
+    /// A VSlam::feeds index
+    feed: usize,
+    /// The keypoints and corresponding descriptors observed on this frame
+    features: Features,
+    /// A map from VSlam::landmarks indices to Frame::features indices
+    landmarks: HashMap<usize, usize>,
+    /// A list of outgoing (first in pair) covisibilities
+    covisibilities: HashSet<Pair>,
+}
+
+impl Frame {
+    fn keypoints(&self) -> impl Iterator<Item = NormalizedKeyPoint> + Clone + '_ {
+        self.features.iter().map(|&(kp, _)| kp)
+    }
+
+    fn descriptors(&self) -> impl Iterator<Item = BitArray<64>> + Clone + '_ {
+        self.features.iter().map(|&(_, d)| d)
+    }
+
+    fn keypoint(&self, ix: usize) -> NormalizedKeyPoint {
+        self.features[ix].0
+    }
+
+    fn descriptor(&self, ix: usize) -> BitArray<64> {
+        self.features[ix].1
+    }
+}
+
+/// The observance of a landmark on a particular frame.
+struct Observance {
+    /// A VSlam::frames index
+    frame: usize,
+    /// A Frame::features index
+    feature: usize,
+}
+
+/// A 3d point in space that has been observed on two or more frames
+struct Landmark {
+    /// Contains the observances of the landmark on various frames
+    observances: Slab<Observance>,
+}
+
+struct Feed {
+    /// The camera intrinsics for this feed
+    intrinsics: CameraIntrinsicsK1Distortion,
+    /// VSlam::frames indices corresponding to each frame of the feed
+    frames: Vec<usize>,
+}
+
+struct Covisibility {
+    /// The relative pose between the first to the second frame in the covisibility
+    pose: RelativeCameraPose,
+    /// The matches in index from the first view to the second
+    matches: Vec<FeatureMatch<usize>>,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Pair(usize, usize);
+
+impl Pair {
+    /// Creates a new pair, cannonicalizing the order of the pair.
+    pub fn new(a: usize, b: usize) -> Self {
+        Self(std::cmp::min(a, b), std::cmp::max(a, b))
+    }
+}
+
+pub struct VSlam<C, EE, T, R> {
+    /// Contains the camera intrinsics for each feed
+    feeds: Slab<Feed>,
+    /// Contains all the landmarks
+    landmarks: Slab<Landmark>,
+    /// Contains all the frames
+    frames: Slab<Frame>,
+    /// Contains a mapping from cannonical pairs to covisibility data
+    covisibilities: HashMap<Pair, Option<Covisibility>>,
+    /// The threshold used for akaze
+    akaze_threshold: f64,
+    /// The threshold distance below which a match is allowed
+    match_threshold: usize,
+    /// The number of points to use in optimization of matches
+    optimization_points: usize,
+    /// The maximum cosine distance permitted in a valid match
+    cosine_distance_threshold: f64,
+    /// The consensus algorithm
+    consensus: RefCell<C>,
+    /// The essential matrix estimator
+    essential_estimator: EE,
+    /// The triangulation algorithm
+    triangulator: T,
+    /// The random number generator
+    rng: RefCell<R>,
+}
+
+impl<C, EE, T, R> VSlam<C, EE, T, R>
+where
+    EE: Estimator<FeatureMatch<NormalizedKeyPoint>, Model = EssentialMatrix>,
+    C: Consensus<EE, FeatureMatch<NormalizedKeyPoint>>,
+    T: TriangulatorObservances + Clone,
+    R: Rng,
+{
+    /// Creates an empty vSLAM reconstruction.
+    pub fn new(consensus: C, essential_estimator: EE, triangulator: T, rng: R) -> Self {
+        Self {
+            feeds: Default::default(),
+            landmarks: Default::default(),
+            frames: Default::default(),
+            covisibilities: Default::default(),
+            akaze_threshold: 0.001,
+            match_threshold: 64,
+            cosine_distance_threshold: 0.001,
+            optimization_points: 16,
+            consensus: RefCell::new(consensus),
+            essential_estimator,
+            triangulator,
+            rng: RefCell::new(rng),
+        }
+    }
+
+    /// Set the akaze threshold.
+    ///
+    /// Default: `0.001`
+    pub fn akaze_threshold(self, akaze_threshold: f64) -> Self {
+        Self {
+            akaze_threshold,
+            ..self
+        }
+    }
+
+    /// Set the match threshold.
+    ///
+    /// Default: `64`
+    pub fn match_threshold(self, match_threshold: usize) -> Self {
+        Self {
+            match_threshold,
+            ..self
+        }
+    }
+
+    /// Set the number of points used for optimization of matching.
+    ///
+    /// Default: `16`
+    pub fn optimization_points(self, optimization_points: usize) -> Self {
+        Self {
+            optimization_points,
+            ..self
+        }
+    }
+
+    /// Set the maximum cosine distance allowed as a match residual.
+    ///
+    /// Default: `0.001`
+    pub fn cosine_distance_threshold(self, cosine_distance_threshold: f64) -> Self {
+        Self {
+            cosine_distance_threshold,
+            ..self
+        }
+    }
+
+    /// Adds a new feed with the given intrinsics.
+    pub fn insert_feed(&mut self, intrinsics: CameraIntrinsicsK1Distortion) -> usize {
+        self.feeds.insert(Feed {
+            intrinsics,
+            frames: vec![],
+        })
+    }
+
+    /// Add frame.
+    ///
+    /// This may perform camera tracking, and thus will take a while.
+    ///
+    /// Returns a VSlam::frames index.
+    pub fn insert_frame(&mut self, feed: usize, image: &DynamicImage) -> usize {
+        let next_id = self.frames.insert(Frame {
+            feed,
+            features: self.kps_descriptors(&self.feeds[feed].intrinsics, image),
+            landmarks: Default::default(),
+            covisibilities: Default::default(),
+        });
+        self.feeds[feed].frames.push(next_id);
+        let feed_frames = &self.feeds[feed].frames[..];
+        if feed_frames.len() >= 2 {
+            let a = feed_frames[feed_frames.len() - 2];
+            let b = feed_frames[feed_frames.len() - 1];
+            self.try_match(Pair::new(a, b));
+        }
+        next_id
+    }
+
+    fn get_pair(&self, Pair(a, b): Pair) -> Option<(&Frame, &Frame)> {
+        Some((self.frames.get(a)?, self.frames.get(b)?))
+    }
+
+    /// Attempts to match the pair.
+    ///
+    /// Returns `true` if successful.
+    fn try_match(&mut self, pair: Pair) -> bool {
+        // First see if the covisibility has already been computed, and if so, don't recompute it.
+        if let Some(outcome) = self
+            .covisibilities
+            .get(&pair)
+            .map(|c| c.as_ref().map(|_| pair))
+        {
+            return outcome.is_some();
+        }
+
+        // Get the two frames.
+        let (a, b) = self.get_pair(pair).expect("tried to get an invalid pair");
+
+        // Generate the outcome.
+        let outcome = self.create_covisibility(a, b);
+        let success = outcome.is_some();
+
+        // Add the outcome.
+        self.add_covisibility_outcome(pair, outcome);
+
+        success
+    }
+
+    fn add_covisibility_outcome(&mut self, pair: Pair, covisibility: Option<Covisibility>) {
+        if let Some(covisibility) = covisibility {
+            // Add it to the covisibility map.
+            self.covisibilities.insert(pair, Some(covisibility));
+            // Add the covisibility to the first frame.
+            self.frames[pair.0].covisibilities.insert(pair);
+        } else {
+            // Put a None to indicate the failure.
+            self.covisibilities.insert(pair, None);
+        }
+    }
+
+    fn create_covisibility(&self, a: &Frame, b: &Frame) -> Option<Covisibility> {
+        // A helper to convert an index match to a keypoint match given frame a and b.
+        let match_ix_kps = |&FeatureMatch(aix, bix)| FeatureMatch(a.keypoint(aix), a.keypoint(bix));
+
+        // Retrieve the matches which agree with each other from each frame and filter out ones that aren't within the match threshold.
+        let matches: Vec<FeatureMatch<usize>> = symmetric_matching(a, b)
+            .filter(|&(_, distance)| distance < self.match_threshold)
+            .map(|(m, _)| m)
+            .collect();
+
+        // Estimate the essential matrix and retrieve the inliers
+        let (essential, inliers) = self.consensus.borrow_mut().model_inliers(
+            &self.essential_estimator,
+            matches
+                .iter()
+                .map(match_ix_kps)
+                .collect::<Vec<_>>()
+                .iter()
+                .copied(),
+        )?;
+        // Reconstitute only the inlier matches into a matches vector.
+        let matches: Vec<FeatureMatch<usize>> = inliers.into_iter().map(|ix| matches[ix]).collect();
+
+        // Perform a chirality test to retain only the points in front of both cameras.
+        let (pose, inliers) = essential
+            .pose_solver()
+            .solve_unscaled_inliers(self.triangulator.clone(), matches.iter().map(match_ix_kps))?;
+        let matches = inliers
+            .iter()
+            .map(|&inlier| matches[inlier])
+            .collect::<Vec<_>>();
+
+        // Select a random sample of 32 points to use for optimization.
+        let opti_matches: Vec<FeatureMatch<NormalizedKeyPoint>> = matches
+            .choose_multiple(&mut *self.rng.borrow_mut(), self.optimization_points)
+            .map(match_ix_kps)
+            .collect();
+
+        let lm = LevenbergMarquardt::new();
+        let pose = lm
+            .minimize(TwoViewOptimizer::new(
+                opti_matches.iter().copied(),
+                pose.0,
+                self.triangulator.clone(),
+            ))
+            .0
+            .pose;
+
+        // Filter outlier matches based on reprojection error.
+        let matches: Vec<FeatureMatch<usize>> = matches
+            .iter()
+            .filter_map(|m| {
+                let FeatureMatch(a, b) = match_ix_kps(m);
+                self.triangulator
+                    .triangulate_relative(pose, a, b)
+                    .map(|point_a| {
+                        let point_b = pose.transform(point_a);
+                        1.0 - point_a.coords.normalize().dot(&a.bearing()) + 1.0
+                            - point_b.coords.normalize().dot(&b.bearing())
+                    })
+                    .and_then(|residual| {
+                        if residual < self.cosine_distance_threshold {
+                            Some(*m)
+                        } else {
+                            None
+                        }
+                    })
+            })
+            .collect();
+
+        // Add the new covisibility.
+        Some(Covisibility { pose, matches })
+    }
+
+    fn kps_descriptors(
+        &self,
+        intrinsics: &CameraIntrinsicsK1Distortion,
+        image: &DynamicImage,
+    ) -> Features {
+        let (kps, ds) = akaze::Akaze::new(self.akaze_threshold).extract(image);
+        kps.into_iter()
+            .zip(ds)
+            .map(|(kp, d)| (intrinsics.calibrate(kp), d))
+            .collect()
+    }
+
+    fn covisibility_keypoint_matches<'a>(
+        &'a self,
+        pair: Pair,
+        covisibility: &'a Covisibility,
+    ) -> impl Iterator<Item = FeatureMatch<NormalizedKeyPoint>> + 'a {
+        covisibility
+            .matches
+            .iter()
+            .map(move |&FeatureMatch(aix, bix)| {
+                FeatureMatch(
+                    self.frames[pair.0].keypoint(aix),
+                    self.frames[pair.1].keypoint(bix),
+                )
+            })
+    }
+
+    pub fn export_covisibility(&self, pair: Pair, path: impl AsRef<Path>) {
+        // Get the covisibility object.
+        let covisibility = self
+            .covisibilities
+            .get(&pair)
+            .expect("covisibility not found")
+            .as_ref()
+            .expect("covisibility was found to have been a failure");
+
+        // Output point cloud.
+        let points: Vec<Point3<f64>> = self
+            .covisibility_keypoint_matches(pair, covisibility)
+            .map(|FeatureMatch(a, b)| {
+                self.triangulator
+                    .triangulate_relative(covisibility.pose, a, b)
+                    .unwrap()
+                    .0
+            })
+            .collect();
+        crate::export::export(std::fs::File::create(path).unwrap(), points);
+    }
+}
+
+fn matching(
+    a_descriptors: impl Iterator<Item = BitArray<64>>,
+    b_descriptors: impl Iterator<Item = BitArray<64>> + Clone,
+) -> Vec<(usize, usize)> {
+    a_descriptors
+        .map(|a| {
+            b_descriptors
+                .clone()
+                .map(|b| a.distance(&b))
+                .enumerate()
+                .min_by_key(|&(_, d)| d)
+                .unwrap()
+        })
+        .collect::<Vec<_>>()
+}
+
+fn symmetric_matching<'a>(
+    a: &'a Frame,
+    b: &'a Frame,
+) -> impl Iterator<Item = (FeatureMatch<usize>, usize)> + 'a {
+    // The best match for each feature in frame a to frame b's features.
+    let forward_matches = matching(a.descriptors(), b.descriptors());
+    // The best match for each feature in frame b to frame a's features.
+    let reverse_matches = matching(b.descriptors(), a.descriptors());
+    forward_matches
+        .into_iter()
+        .enumerate()
+        .filter_map(move |(aix, (bix, distance))| {
+            // Does the feature in b match with this feature too?
+            let is_symmetric = reverse_matches[bix].0 == aix;
+            if is_symmetric {
+                Some((FeatureMatch(aix, bix), distance))
+            } else {
+                None
+            }
+        })
+}
