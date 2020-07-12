338895c44ed7f46e604916bef1b9cd52c0fc9d7a
Geordon Worley <vadixidav@gmail.com>
2020-07-11 21:18:38 -0400

    add ManyViewConstraint

--- src/many_view_optimizer.rs
+++ src/many_view_optimizer.rs
@@ -1,9 +1,155 @@
+use argmin::{
+    core::{ArgminOp, Error},
+    solver::neldermead::NelderMead,
+};
+use average::Mean;
 use cv_core::nalgebra::{
     dimension::{Dynamic, U1, U4, U6},
     DMatrix, DVector, Matrix3, VecStorage, Vector4, Vector6,
 };
 use cv_core::{Bearing, Pose, Projective, TriangulatorObservances, WorldPoint, WorldToCamera};
 use levenberg_marquardt::LeastSquaresProblem;
+use ndarray::{s, Array2};
+
+pub fn many_view_nelder_mead(poses: Vec<WorldToCamera>) -> NelderMead<Array2<f64>, f64> {
+    let num_poses = poses.len();
+    let se3s: Vec<Vector6<f64>> = poses.iter().map(|p| p.se3()).collect();
+    let original = Array2::from_shape_vec(
+        (num_poses, 6),
+        se3s.iter().flat_map(|se3| se3.iter().copied()).collect(),
+    )
+    .expect("failed to convert poses into Array2");
+    let translation_scale: Mean = original
+        .outer_iter()
+        .map(|pose| {
+            pose.slice(s![0..3])
+                .iter()
+                .map(|n| n.powi(2))
+                .sum::<f64>()
+                .sqrt()
+                * 0.5
+        })
+        .collect();
+    let mut variants = vec![original; num_poses * 6 + 1];
+    #[allow(clippy::needless_range_loop)]
+    for i in 0..num_poses * 6 {
+        let pose = i / 6;
+        let subi = i % 6;
+        if subi < 3 {
+            // Translation simplex must be relative to existing translation.
+            variants[i][(pose, subi)] += translation_scale.mean();
+        } else {
+            // Rotation simplex must be kept within a small rotation (2 pi would be a complete revolution).
+            variants[i][(pose, subi)] += std::f64::consts::PI * 0.01;
+        }
+    }
+    NelderMead::new().with_initial_params(variants)
+}
+
+#[derive(Clone)]
+pub struct ManyViewConstraint<B, T> {
+    loss_cutoff: f64,
+    landmarks: Vec<Vec<Option<B>>>,
+    triangulator: T,
+}
+
+impl<B, T> ManyViewConstraint<B, T>
+where
+    B: Bearing + Clone,
+    T: TriangulatorObservances,
+{
+    /// Creates a ManyViewConstraint.
+    ///
+    /// Note that `landmarks` is an iterator over each landmark. Each landmark is an iterator over each
+    /// pose presenting the occurence of that landmark in the pose's view. If the landmark doesn't appear in the
+    /// view of the pose, the iterator should return `None` for that observance, and Some with the bearing otherwise.
+    ///
+    /// A landmark is often called a "track" by other MVG software, but the term landmark is preferred to avoid
+    /// ambiguity between "camera tracking" and "a track".
+    pub fn new<L, O>(landmarks: L, triangulator: T) -> Self
+    where
+        L: Iterator<Item = O> + Clone,
+        O: Iterator<Item = Option<B>>,
+        T: TriangulatorObservances,
+    {
+        let landmarks = landmarks.map(|observances| observances.collect()).collect();
+        Self {
+            loss_cutoff: 0.01,
+            landmarks,
+            triangulator,
+        }
+    }
+
+    pub fn loss_cutoff(self, loss_cutoff: f64) -> Self {
+        Self {
+            loss_cutoff,
+            ..self
+        }
+    }
+
+    pub fn residuals<'a>(
+        &'a self,
+        poses: impl Iterator<Item = WorldToCamera> + Clone + 'a,
+    ) -> impl Iterator<Item = f64> + 'a {
+        let loss = move |n: f64| {
+            if n > self.loss_cutoff {
+                self.loss_cutoff
+            } else {
+                n
+            }
+        };
+        let poses_res = poses.clone();
+        self.landmarks
+            .iter()
+            .filter_map(move |observations| {
+                let point = self.triangulator.triangulate_observances(
+                    poses
+                        .clone()
+                        .zip(observations)
+                        .filter_map(|(pose, observance)| {
+                            // Create a tuple of the pose with its corresponding observance.
+                            observance.clone().map(move |observance| (pose, observance))
+                        }),
+                )?;
+                Some((point, observations))
+            })
+            .flat_map(move |(pw, lms)| {
+                poses_res.clone().zip(lms.iter()).map(move |(pose, lm)| {
+                    // TODO: Once try blocks get added, this should be replaced with a try block.
+                    let res = || -> Option<f64> {
+                        let pc = pose.transform(pw);
+                        Some(loss(1.0 - lm.as_ref()?.bearing().dot(&pc.bearing())))
+                    };
+                    res().unwrap_or(0.0)
+                })
+            })
+    }
+}
+
+impl<B, T> ArgminOp for ManyViewConstraint<B, T>
+where
+    B: Bearing + Clone,
+    T: TriangulatorObservances,
+{
+    type Param = Array2<f64>;
+    type Output = f64;
+    type Hessian = ();
+    type Jacobian = ();
+    type Float = f64;
+
+    fn apply(&self, p: &Self::Param) -> Result<Self::Output, Error> {
+        let poses: Vec<WorldToCamera> = p
+            .outer_iter()
+            .map(|pose_arr| {
+                Pose::from_se3(Vector6::from_row_slice(
+                    pose_arr.as_slice().expect("param was not contiguous array"),
+                ))
+            })
+            .collect();
+        let mean: Mean = self.residuals(poses.iter().copied()).collect();
+        Ok(mean.mean())
+    }
+}
 
 #[derive(Clone)]
 pub struct ManyViewOptimizer<B> {
