6d8d91a0f0a5076927a3666c8192110abfb9d24b
Remco Bloemen <remco@0x.org>
2020-11-02 11:35:53 -0800

    Rational inverse

--- cv-pinhole/src/distortion_function/rational.rs
+++ cv-pinhole/src/distortion_function/rational.rs
@@ -1,6 +1,6 @@
 use super::DistortionFunction;
-use cv_core::nalgebra::DimAdd;
-use cv_core::nalgebra::DimSum;
+use cv_core::nalgebra::{DimAdd, DimSum};
+use num_traits::Float;
 
 #[derive(Clone, Debug, Default)]
 struct Rational<P, Q>(P, Q)
@@ -32,7 +32,41 @@ where
         (p / q, (dp * q - p * dq) / (q * q))
     }
 
+    /// Numerically invert the function.
+    ///
+    /// # Method
+    ///
+    /// Starting with the function definition:
+    ///
+    /// $$
+    /// y = \frac{P(x)}{Q(x)}
+    /// $$
+    ///
+    /// We manipulate this into a root finding problem linear in $P$, $Q$ which
+    /// gives $y ⋅ Q(x) - P(x) = 0$. To solve this we use the Newton-Raphson
+    /// method with $x_0 = y$ and
+    ///
+    /// $$
+    /// x_{i+1} = x_i - \frac{y ⋅ Q(x) - P(x)}{y ⋅ Q'(x) - P'(x)}
+    /// $$
+    ///
     fn inverse(&self, value: f64) -> f64 {
-        todo!()
+        // Maxmimum number of iterations to use in Newton-Raphson inversion.
+        const MAX_ITERATIONS: usize = 100;
+
+        // Convergence treshold for Newton-Raphson inversion.
+        const EPSILON: f64 = f64::EPSILON;
+
+        let mut x = value;
+        for _ in 0..MAX_ITERATIONS {
+            let (p, dp) = self.0.with_derivative(x);
+            let (q, dq) = self.1.with_derivative(x);
+            let delta = (value * q - p) / (value * dq - dp);
+            x -= delta;
+            if Float::abs(delta) <= EPSILON * x {
+                break;
+            }
+        }
+        x
     }
 }
