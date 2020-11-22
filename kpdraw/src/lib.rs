c4af94bb0f83f5b3a43919742be0600f7e43fa98
Geordon Worley <vadixidav@gmail.com>
2020-11-22 16:20:45 -0500

    fix deprecated function lint

--- kpdraw/src/lib.rs
+++ kpdraw/src/lib.rs
@@ -5,7 +5,7 @@ use imageproc::drawing;
 pub fn render_akaze_keypoints(image: &DynamicImage, threshold: f64) -> DynamicImage {
     let akaze = Akaze::new(threshold);
     let (kps, _) = akaze.extract(image);
-    let mut image = drawing::Blend(image.to_rgba());
+    let mut image = drawing::Blend(image.to_rgba8());
     for KeyPoint { point: (x, y), .. } in kps {
         drawing::draw_cross_mut(&mut image, Rgba([0, 255, 255, 128]), x as i32, y as i32);
     }
