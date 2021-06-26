96fbdacea60ddb2abf03dfacf7fcaf9f342e3d2b
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2021-06-26 12:41:02 +0200

    Fix random generation in range in tutorial

--- tutorial-code/chapter2-first-program/src/main.rs
+++ tutorial-code/chapter2-first-program/src/main.rs
@@ -7,8 +7,8 @@ fn main() {
     let mut rng = rand::thread_rng();
     let mut canvas = drawing::Blend(src_image.to_rgba8());
     for _ in 0..50 {
-        let x = rng.gen_range(0, src_image.width()) as i32;
-        let y = rng.gen_range(0, src_image.height()) as i32;
+        let x = rng.gen_range(0..src_image.width()) as i32;
+        let y = rng.gen_range(0..src_image.height()) as i32;
         drawing::draw_cross_mut(&mut canvas, Rgba([0, 255, 255, 128]), x, y);
     }
 
