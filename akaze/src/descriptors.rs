3020354a204667739a941baf3e48a4d0f3fa68f7
Stéphane Magnenat <stephane@magnenat.net>
2023-06-12 17:11:12 +0200

    Akaze: Optionally use rayon for extracting descriptors.

--- akaze/src/descriptors.rs
+++ akaze/src/descriptors.rs
@@ -1,6 +1,9 @@
 use crate::{Akaze, Error, EvolutionStep, KeyPoint};
 use bitarray::BitArray;
 
+#[cfg(feature = "rayon")]
+use rayon::prelude::*;
+
 impl Akaze {
     /// Extract descriptors from keypoints/an evolution
     ///
@@ -15,6 +18,7 @@ impl Akaze {
         evolutions: &[EvolutionStep],
         keypoints: &[KeyPoint],
     ) -> (Vec<KeyPoint>, Vec<BitArray<64>>) {
+        #[cfg(not(feature = "rayon"))] {
         keypoints
             .iter()
             .filter_map(|&keypoint| {
@@ -24,6 +28,18 @@ impl Akaze {
                 ))
             })
             .unzip()
+        }
+        #[cfg(feature = "rayon")] {
+        keypoints
+            .par_iter()
+            .filter_map(|&keypoint| {
+                Some((
+                    keypoint,
+                    self.get_mldb_descriptor(&keypoint, evolutions).ok()?,
+                ))
+            })
+            .unzip()
+        }
     }
 
     /// Computes the rotation invariant M-LDB binary descriptor (maximum descriptor length)
